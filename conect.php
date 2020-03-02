0fc03c1a169306ef84f6af12ab5a621b1f086c07
Yury Trindade <47568863+makaifox@users.noreply.github.com>
2020-03-01 21:06:54 -0300

    Add files via upload

--- conect.php
+++ conect.php
@@ -1,11 +1,6 @@
 <?php 
-
 //conectando com o localhost - mysql
 //iniciando a conexão com o banco de dados 
-//Banco criado por MySQL (phpMyadmin)
-//host: localhost 
-//usuario: root
-//Senha: 123
 $cx = mysqli_connect("localhost", "root", "123");
  
 //selecionando o banco de dados 
